5e1192645b5270bec68804ee34b33112245c5452
marc.friedli <marc.friedli@he-arc.ch>
2017-02-28 14:57:27 +0100

    Création du dossier du permier labo + code permettant de cast un nombre sous forme '2^x * y' où x est un nombre entier et 0.5<=y<1

---
+++ Labo1/test_algo_nbre_log_deux.php
@@ -0,0 +1,40 @@
+<!--
+Developped by team A2 :
+Florian Fasmeyer,
+Marc Friedli,
+Paul Jeanbourquin
+-->
+<!DOCTYPE html  PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html>
+
+<head>
+	<title>Test</title>
+	<link rel="stylesheet" href="style.css">
+
+</head>
+<body>
+
+<div id="title">
+	<h1>Entrez un nombre !</h1>
+</div>
+
+
+
+<div>
+	<button type="button" id="myBtn" onClick="returnFloatNumber()">Cliquez !</button>
+</div>
+
+<div id="Anser">
+</div>
+</body>
+</html>
+
+<script>
+function returnFloatNumber(){
+	var number=parseFloat(prompt("Nbre ?"));
+	var logTwo = Math.log2(number);
+	var exposant = Math.floor(logTwo)+1;
+	var mantisse = number/Math.pow(2,exposant);
+	document.getElementById("Anser").innerHTML="Le nombre peut s'écrire comme étant 2^"+exposant+" * "+mantisse;
+}
+</script>
