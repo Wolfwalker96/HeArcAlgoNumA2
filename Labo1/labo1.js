55cfeef963e47894279ab175108079c05f930ea7
marc.friedli <marc.friedli@he-arc.ch>
2017-03-01 11:15:39 +0100

    création d'une page contenant notre projet. Création d'une seconde page contantn le code JS. Actuellement, le projet est encore sous forme de prototype. Il cast un nombre choisi par l'utilisateur sous sa forme float 37 bits. Il est cenpendant à noter qu'il ne fonctionne pas pour toutes les valeurs (C.F. README pour plus d'infos

---
+++ Labo1/labo1.js
@@ -0,0 +1,60 @@
+/* Déclaration des variables */
+var mantisse;
+var exposant;
+var signe;
+var binaryNumber = [37]
+
+function castToBinary(nbrBits, deciNumber){
+	var binaryArray = [nbrBits];
+	for(var i= nbrBits-1; i>=0;i--){
+		var pow= Math.pow(2,i);
+		if(deciNumber>=pow){
+			deciNumber-=pow;
+			binaryArray[i]=1;
+		}
+		else{
+			binaryArray[i]=0;
+		}
+	}
+	return binaryArray;
+}
+
+//Fonction qui cast le nombre sous sa forme binaire
+function returnBinaryFloatNumber(){
+	mantisse*=1000000;
+	var binaryMantisse = castToBinary(28,mantisse);
+	var binaryExposant = castToBinary(9,exposant);
+	binaryMantisse.reverse();
+	binaryExposant.reverse();
+	var i;
+	for(i = 0; i<9;i++){
+		binaryNumber[i+1] = binaryExposant[i];
+	}
+	for(i=1;i<28;i++){
+		binaryNumber[i+9] = binaryMantisse[i];
+	}
+	document.getElementById("Anser").innerHTML=binaryNumber;
+}
+
+function determineExposantAndMantisse(){
+	var number=parseFloat(prompt("Nbre ?"));
+
+	//Détermine le bit du signe
+	if(number<0){
+		binaryNumber[0]=1;
+		number*=-1;
+	}
+	else{
+		binaryNumber[0]=0;
+	}
+
+	//Calcule l'exposant (sous forme décimale)
+	var logTwo = Math.log2(number);
+	exposant = Math.floor(logTwo)+1;
+
+	//Calcule la mantisse (sous forme décimale)
+	mantisse = number/Math.pow(2,exposant);
+
+	returnBinaryFloatNumber();
+	//document.getElementById("Anser").innerHTML="Le nombre peut s'écrire comme étant 2^"+exposant+" * "+mantisse;
+}
