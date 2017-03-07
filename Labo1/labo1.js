757d3a738b9a56cea6e0b7e89398094f05fe79f0
marc.friedli <marc.friedli@he-arc.ch>
2017-03-07 17:17:48 +0100

    refactoring : les méthodes permettant de trouver le float n'étaient pas très adaptés à la pseudo-classe

--- Labo1/labo1.js
+++ Labo1/labo1.js
@@ -42,16 +42,16 @@ function TeamTwo(){
 
 /* Déclaration des variables */
 //struc samere{
-var exposant;
 var binaryMantisse=[28];
 var binaryNumber = [37];
 var binaryExposant=[9];
+var signe;
 //}
 
 function castToBinary(nbrBits, deciNumber){
-	var binaryArray = [nbrBits];
+	let binaryArray = [nbrBits];
 	for(var i= nbrBits-1; i>=0;i--){
-		var pow= Math.pow(2,i);
+		let pow= Math.pow(2,i);
 		if(deciNumber>=pow){
 			deciNumber-=pow;
 			binaryArray[i]=1;
@@ -72,7 +72,6 @@ function returnBinryMantisse(mantisse){
 		if(1/denominator<=mantisse){
 			binaryMantisse[i]=1;
 			mantisse-=1/denominator;
-			//alert(1/denominator + " " + i);
 		}
 		else{
 			binaryMantisse[i]=0;
@@ -83,51 +82,38 @@ function returnBinryMantisse(mantisse){
 }
 
 //Fonction qui cast le nombre sous sa forme binaire
-function returnBinaryFloatNumber(){
+function returnBinaryExp(exposant){
 	binaryExposant = castToBinary(9,exposant);
 	binaryExposant.reverse();
-	let i;
-	for(i = 0; i<9;i++){
-		binaryNumber[i+1] = binaryExposant[i];
-	}
-	for(i=1;i<28;i++){
-		binaryNumber[i+9] = binaryMantisse[i];
-	}
-	//Affichage
-	//document.getElementById("Anser").innerHTML=binaryNumber;
 }
 
 function determineExposantAndMantisse(){
-	var number=parseFloat(prompt("Nbre ?"));
+	let number=parseFloat(prompt("Nbre ?"));
 
 	//Détermine le bit du signe
 	if(number<0){
-		binaryNumber[0]=1;
+		signe=1;
 		number*=-1;
 	}
 	else{
-		binaryNumber[0]=0;
+		signe=0;
 	}
 
 	//Calcule l'exposant (sous forme décimale)
 	var logTwo = Math.log2(number);
-	exposant = Math.floor(logTwo)+1;
+	let exposant = Math.floor(logTwo)+1;
 
 	//Calcule la mantisse (sous forme décimale)
 	let mantisse = number/Math.pow(2,exposant);
 
 	returnBinryMantisse(mantisse);
-	returnBinaryFloatNumber();
+	returnBinaryExp(exposant);
 
 	var dumb = new TeamTwo();
-	dumb.setSgn(binaryNumber[0]);
+	dumb.setSgn(signe);
 	dumb.setFrac(binaryMantisse);
 	dumb.setExp(binaryExposant);
 	dumb.displayBasic("debug");
 	//Affichage
-	//document.getElementById("Decimal").innerHTML="Le nombre peut s'écrire comme étant 2^"+exposant+" * "+mantisse;
-}
-
-function floatAdd(){
-
+	document.getElementById("Decimal").innerHTML="Le nombre peut s'écrire comme étant 2^"+exposant+" * "+mantisse;
 }
