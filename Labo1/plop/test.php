dd44a9b911d7b93c2174c772f422f54c62c2cc8f
florian.fasmeyer <florian.fasmeyer@he-arc.ch>
2017-03-09 20:51:14 +0100

    better script -> test.php | be happy, please

--- Labo1/plop/test.php
+++ Labo1/plop/test.php
@@ -1,24 +1,27 @@
-<script>
-<?php 
-	require_once("arrayFunc.js");
-	require_once("typeTeamTwo.js");
-?>
-var dump = new TeamTwo();
-dump.sgn = 0;
-fill(dump.exp, 1);
-fill(dump.mant, 0);
+<html>
+<head>
+</head>
+<body>
+	<script src="typeTeamTwo.js" ></script>
+	<script src="arrayFunc.js" ></script>
+	<script>
+		var dump = new TeamTwo();
+		dump.sgn = 1;
+		fill(dump.exp, 1);
+		fill(dump.mant, 1);
 
-var dada = dump.getNumeric();
-document.write(dada + "</br>");
+		var dada = dump.getNumeric();
+		document.write(dada + "</br>");
 
-document.write("End file");
+		document.write("End file");
 
 
-/*
-	Merci de faire les tests ainsi.
-		instant -> F5 (pas de presse bouton)
-		easy	-> change 2 var
-		propre  -> propre
-*/
-	
-</script>
+		/*
+			Merci de faire les tests ainsi.
+				instant -> F5 (pas de presse bouton)
+				easy	-> change 2 var
+				propre  -> propre
+		*/
+	</script>
+</body>
+</html>
