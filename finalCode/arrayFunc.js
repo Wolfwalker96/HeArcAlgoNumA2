910ef9b4d02d10b3c88ffffc3ccc4d5ab5ceb574
florian.fasmeyer <florian.fasmeyer@he-arc.ch>
2017-03-10 00:01:43 +0100

    final code, use it, it's clean and easy to test!

---
+++ finalCode/arrayFunc.js
@@ -0,0 +1,23 @@
+/*
+	Simple array operations
+	Numeric Algorithm course, team2 
+	HES-SO HE-ARC - 09.03.2017
+	
+	Marc Friedli
+	Paul Ami Jean-Bourquin
+	Florian Fasmeyer
+*/ 
+function filledArray(array, value){	//If 'array' filled with 'value' return 1
+	for(let i=0; i<array.length; i++){
+		if(array[i] !== value){
+			return 0;
+		}
+	}
+	return 1;
+}
+function fill(array, value){		//Fill 'array' with 'value'
+	for (let i=0; i<array.length; i++) {
+		array[i] = value;
+	}
+	return array;	//not required
+}
