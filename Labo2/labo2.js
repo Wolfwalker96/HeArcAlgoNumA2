a5de8395529c5a017d039b0c9c6ecbd5e4b814d2
paul.jeanbour <pa.jeanbourquin@gmail.com>
2017-03-14 14:04:50 +0100

    Root review
    Issues:
    
    * Infinite loop

--- Labo2/labo2.js
+++ Labo2/labo2.js
@@ -9,13 +9,24 @@ var func2 = function(x){
   return x/(1-(x*x));
 }
 
+
+// z(x)
+var gen1 = function(x){
+  return Math.sin(x)-x/13;
+}
+
+// y(x)
+var gen2 = function(x){
+  return x/(1-(x*x));
+}
+
 // f(x)=g(x)
-function Racine(g,f){
+function Racine(f,g,a,b){
   // Algo de résolution
-
-  var a = -100 ;
-  var b = 0;
-
+  /*
+  var a = 2 ;
+  var b = 100;
+  */
   // Une fonction dans une fonction... Le JS c'est beau :)
   function h(x){ return f(x)-g(x); }
 
@@ -41,8 +52,12 @@ function Racine(g,f){
 }
 
 // Récuper les données et execute
-function Execute(){
-  console.log(Racine(func1,func2));
+function Execute(a,b,fin=100){
+  c=Racine(func1,func2,a,b);
+  console.log(c);
+  if(fin==0) return "pommes";
+  Execute(a,c,fin-1);
+  Execute(c,b,fin-1);
 }
 
 // Valide le formulaire
@@ -51,4 +66,4 @@ function ValidateForm(){
 }
 
 // Execution automatique
-Execute();
+Execute(-100,100);
