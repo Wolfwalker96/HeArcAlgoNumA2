e1da7c663327cc16ed110d9d7e190bbba8b0f39c
paul.jeanbour <pa.jeanbourquin@gmail.com>
2017-03-14 22:33:59 +0100

    try something with Explorer
    Creating an Explorer function who analyse the sign in the interval and
    calculate the roots when it's needed

--- Labo2/labo2.js
+++ Labo2/labo2.js
@@ -21,12 +21,12 @@ var gen2 = function(x){
 }
 
 // f(x)=g(x)
-function Racine(f,g,a,b){
+function Racine(f,g,a2,b2){
   // Algo de résolution
-  /*
-  var a = 2 ;
-  var b = 100;
-  */
+
+  var a = a2 ;
+  var b = b2;
+
   // Une fonction dans une fonction... Le JS c'est beau :)
   function h(x){ return f(x)-g(x); }
 
@@ -34,12 +34,12 @@ function Racine(f,g,a,b){
   var mold = 2*mnew;
 
   while(mnew-mold!=0){
-    console.log("mnew = "+mnew);
-    console.log("mold = "+mold);
+    //console.log("mnew = "+mnew);
+    //console.log("mold = "+mold);
     mold = mnew;
     mnew = (a+b)/2
-    console.log("a = "+a);
-    console.log("b = "+b);
+    //console.log("a = "+a);
+    //console.log("b = "+b);
     var k = h(a);
     var m = h(mnew);
     if(m*k<=0) b = mnew;
@@ -60,10 +60,26 @@ function Execute(a,b,fin=100){
   Execute(c,b,fin-1);
 }
 
+function Explorer(a,b,f,g){
+  function h(x){ return f(x)-g(x); }
+  var oldValue=h(a);
+  var oldIndex=a;
+  for(i=a+0.1;i<=b;i+=0.1){
+    var newValue=Math.sign(h(i));
+    if(newValue!=oldValue){
+      console.log("a = "+oldIndex+" b = "+i);
+      console.log(Math.fround(Racine(f,g,oldIndex,i)));
+      oldValue=newValue;
+    }
+    oldIndex=i;
+  }
+}
+
 // Valide le formulaire
 function ValidateForm(){
 
 }
 
 // Execution automatique
-Execute(-100,100);
+//Execute(-100,100);
+Explorer(-100,100,func1,func2);
