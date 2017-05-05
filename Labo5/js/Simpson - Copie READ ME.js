a92d65901318c52099231be430f89ffb6843a386
florian.fasmeyer <florian.fasmeyer@he-arc.ch>
2017-05-05 19:19:14 +0200

    Algorithm done and working, gives an accuracy of 1E-19 in 200ms

--- Labo5/js/Simpson - Copie READ ME.js
+++ Labo5/js/Simpson - Copie READ ME.js
@@ -4,57 +4,61 @@
  *   02.05.2017
  */
 /*
+	READ ME
+	La librairie math.js rend le code illisible. Je vous laisse cette
+	copie au cas ou vous auriez l'urge de modifier du code. hf, gl.
+	Je comptes aussi faire des tests d'opti voir si je trouve pas
+	mieux que math.js, je vais avoir besoin de ce fichier.
+*/
+
+
+/*
 	Simpson method(function, start, stop, nbStep) where start and stop
 	are equal to 0 and 1 respectively. 	f:funciton 	n: nbStep
 	
 	WARNING: 'n' must be even!
 */	
 function simpson(f, n){
-	let h = math.divide(math.bignumber(1), math.bignumber(n)); // Step size.
+	let h = 1/n; // Step size.
 	
-	let sum1=math.bignumber(0);
-	let sum2=math.bignumber(0);
-	let mult=math.bignumber(0);
+	let sum1=0;
+	let sum2=0;
+	let mult=0;
 
 	for(let i=1; i<n; i=i+2){
-		mult = math.multiply(h, i);
-		sum1 = math.add(sum1, f(mult));
-		sum2 = math.add(sum2, f(math.add(mult, -h)));
+		mult = h*i;
+		sum1 += f(mult);
+		sum2 += f(mult-h);
 	}
 	/*
 		Note: we must replace f(1) by the values
 		of f(b=1) wich is 1/2. Made for optimisation purposes.
 	*/
-	return ( math.multiply(math.divide(h,3),
-	( math.add(math.add(-0.5, math.multiply(4, sum1)), math.multiply(2, sum2)))));
+	return ( (h/3) * ( -0.5 + 4*sum1 + 2*sum2) );
 }
 
 /*
 	Replacer le return par la fonction(fait)
 */
 function integral(x){
-	return math.divide(1,math.add(1, math.multiply(x, x)));
+	return 1/(1+x*x);
 }
 /*
 	Using "High Resolution Time API".
 	https://www.sitepoint.com/discovering-the-high-resolution-time-api/
 	*/
 function evaluateTime(){
-	math.config({
-		number: 'BigNumber',
-		precision: 20
-	});
 
-	let temp = math.bignumber(0);
-	let Pi = math.PI;
+	let temp;
+	let Pi = 3.14159265358979323;
 	
 	let avgTime=0;
-	let n=10;
+	let n=10000;
 	for(let me=0; me <n; me++){
 		//start timer
 		let timer = performance.now();
 		//run
-		temp = simpson(integral, 1E4);
+		temp = simpson(integral, 1E3);
 		//end timer
 		timer = performance.now()-timer;
 		avgTime += timer;
@@ -63,9 +67,13 @@ function evaluateTime(){
 	console.log("Avg runtime over "+n+" occurences: "+avgTime.toFixed(4)+" ms");
 	console.log();
 	
-	console.log("Pi:      "+Pi.toString().substring(0,19));
-	console.log("Result:  "+math.multiply(temp, 4).toString().substring(0,19));
-	console.log("Diff:    "+math.add(math.multiply(temp, -4), Pi).toString().substring(0,19));
-
-	return math.multiply(temp, 4).toString().substring(0,19);
+	console.log("Pi:      "+Pi);
+	console.log("Result:  "+temp*4);
+	console.log("Diff:    "+((temp*4)-Pi));
+	
+	let timer = performance.now();
+	console.log(math.bignumber(1).toString());
+	timer = performance.now()-timer;
+	
+	return temp*4;
 }
